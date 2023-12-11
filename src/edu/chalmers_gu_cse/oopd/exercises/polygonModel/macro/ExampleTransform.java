acb327738834b2a0df3a6cdba883565114eee465
bllnr <52850477+bllnr@users.noreply.github.com>
2023-12-11 15:04:08 +0100

    transform

--- src/edu/chalmers_gu_cse/oopd/exercises/polygonModel/macro/ExampleTransform.java
+++ src/edu/chalmers_gu_cse/oopd/exercises/polygonModel/macro/ExampleTransform.java
@@ -1,8 +1,18 @@
 package edu.chalmers_gu_cse.oopd.exercises.polygonModel.macro;
 
+import edu.chalmers_gu_cse.oopd.exercises.polygonModel.polygon.Polygon;
+
 /**
  * Created by Niklas on 2016-03-02.
  */
 public class ExampleTransform implements Transform {
     // TODO: Step 2b: Implement an example of a Transform class
+
+    @Override
+    public Polygon transformPolygon(Polygon p, double radians, double doubleX, double doubleY, int intX, int intY) {
+        p.rotate(radians);
+        p.scale(doubleX, doubleY);
+        p.translate(intX,intY);
+        return p;
+    }
 }
--- src/edu/chalmers_gu_cse/oopd/exercises/polygonModel/macro/Transform.java
+++ src/edu/chalmers_gu_cse/oopd/exercises/polygonModel/macro/Transform.java
@@ -1,10 +1,12 @@
 package edu.chalmers_gu_cse.oopd.exercises.polygonModel.macro;
 
+import edu.chalmers_gu_cse.oopd.exercises.polygonModel.polygon.Polygon;
+
 /**
  * Created by Niklas on 2016-03-02.
  */
 //@FunctionalInterface
 public interface Transform {
     // TODO: Step 2a: What is the representation of a transform?
-    // ... transform(...)
+    Polygon transformPolygon(Polygon p, double radians, double doubleX, double doubleY, int intX, int intY);
 }
